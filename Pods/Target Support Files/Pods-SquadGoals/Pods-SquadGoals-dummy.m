8fd47588d090e0a07eb113a1a4196feeca21e1ce
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-06-02 12:51:39 -0600

    Firebasing

---
+++ Pods/Target Support Files/Pods-SquadGoals/Pods-SquadGoals-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SquadGoals : NSObject
+@end
+@implementation PodsDummy_Pods_SquadGoals
+@end
--- SquadGoals/ViewController.swift
+++ SquadGoals/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var signInTapped: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
